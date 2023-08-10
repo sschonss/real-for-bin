90ecad11d7c73c0616b0739f248591bf746bf7db
Eliezer Boeira <eliezerbuczak2016@gmail.com>
2023-08-10 17:38:46 -0300

    layout alterado e tratado erros

--- src/script.js
+++ src/script.js
@@ -1,25 +1,16 @@
-document
-  .getElementById("form-conversor")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
 
-
-    let bit = document.getElementById('bit').value;
-    decimalForBit(bit);
-
-
-  });
-
-
-
+const converter = () => {
+  let bit = document.getElementById('real').value;
+  decimalForBit(bit);
+}
 
 function decimalForBit(valor) {
 
   let arrayBit = [];
   let isdivisible = true;
 
-  if (valor == 1) {
-    document.getElementById("resultado").innerHTML = valor;
+  if (valor == 1 || valor == 0) {
+    document.getElementById("bit").value = 1;
     return
   }
 
@@ -32,5 +23,20 @@ function decimalForBit(valor) {
       isdivisible = false
     }
   } while (isdivisible === true)
-  document.getElementById("resultado").innerHTML = '' + arrayBit.reverse().join('')
+  document.getElementById("bit").value = '' + arrayBit.reverse().join('')
+}
+
+function bitForDecimal(valor) {
+  let arrayBit = [];
+  for (let i = valor.length; i > 0; i--) {
+    arrayBit.push(valor[i - 1]);
+
+  }
+
+  let resultado = 0;
+  for (let i = 0; i < arrayBit.length; i++) {
+    resultado += arrayBit[i] * Math.pow(2, i)
+  }
+
+  document.getElementById("resultado").innerHTML = resultado;
 }
